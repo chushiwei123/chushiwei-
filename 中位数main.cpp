b1bad7faff2d1c42b42ab8196fe652e296aa206a
chushiwei123 <77722440+chushiwei123@users.noreply.github.com>
2021-01-25 09:07:33 +0800

    Add files via upload

---
+++ 中位数main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	typedef struct ListNode {
+		int val;
+		ListNode *next;
+		
+	};
+	return 0;
+}
